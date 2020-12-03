b5947171a77eb847299c3170bae80e40618d2d82
rjcmagnaye <rjcmagnaye@gmail.com>
2020-12-02 20:38:37 -0700

    Added a list

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Todo' });
+  const items = [
+    {id:1 , description: 'Go To Store'},
+    {id:2 , description: 'Go To School'},
+    {id:3 , description: 'Wash hair'},
+    {id:4 , description: 'Brush Teeth'},
+    {id:5 , description: 'Finish Project'}
+  ];
+  res.render('index', { title: 'Todo', items: items });
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req);
+  res.redirect('/');
 });
 
 router.get('/about', function(req, res, next) {

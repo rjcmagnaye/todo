782129bdf8a9938e19465ee591ef916e79a031e4
rjcmagnaye <rjcmagnaye@gmail.com>
2020-12-09 21:24:11 -0700

    install mongodb

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,23 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const items = [
-    {id:1 , description: 'Go To Store'},
-    {id:2 , description: 'Go To School'},
-    {id:3 , description: 'Wash hair'},
-    {id:4 , description: 'Brush Teeth'},
-    {id:5 , description: 'Finish Project'},
-    {id:6, description: 'Start Project'}
+    { id: 1, description: 'Go to Store'},
+    { id: 2, description: 'Go to School'},
+    { id: 3, description: 'Wash hair'},
+    { id: 4, description: 'Brush teeth'},
+    { id: 5, description: 'Finish Project'},
+    { id: 6, description: 'Start Project'},
   ];
+
   res.render('index', { title: 'Todo', items: items });
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req);
+  console.log(req.body.item);
   res.redirect('/');
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Build Guide' });
+  res.render('about', { title: 'Todo About' });
 });
-
 module.exports = router;
